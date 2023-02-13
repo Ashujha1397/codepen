620abf6810f3336c6717256da090c9517a228b77
ashu <golu17012001@gmail.com>
2023-02-14 01:39:12 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import Home from './components/Home';
+
+import DataProvider from './context/DataProvider';
+
+
+
+
+
+function App() {
+  return (
+    <>
+   <DataProvider>
+
+  
+  
+   <Home/>
+   </DataProvider>
+    </>
+  );
+}
+
+export default App;
